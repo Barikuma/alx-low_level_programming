130d16bf75bd974ad611368bb78ef8e42d62ec33
Barikuma <barikumamondelo@gmail.com>
2022-09-09 23:57:15 +0000

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int i, j, k, l;
+	int i, j, k, l;	
 	
 	i = 0;
 
 	for (i = i ; i < 10 ; i++)
 	{
-		for (j = i + 1 ; j < 9 ; j++)
+		for (j = 0 ; j < 9 ; j++)
 		{
-			for (k = j + 1 ; k < 10 ; k++)
+			for (k = 0 ; k < 10 ; k++)
 			{
-				for (l = k + 1 ; l < 10 ; l++)
+				for (l = i + 1 ; l < 10 ; l++)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
