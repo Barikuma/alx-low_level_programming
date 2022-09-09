82e92de2612990cc6fd17ac18b84d199265d0773
Barikuma <barikumamondelo@gmail.com>
2022-09-09 20:28:31 +0000

    6-print_numberz

--- 0x01-variables_if_else_while/test.c
+++ 0x01-variables_if_else_while/test.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 
 int main(void){
-	int i;
+	int i,j,ch,decimal,count;
+	int temp;
 
-	for (i = 97 ; i < 107 ; i++){
-		if (((char)i) == 'b'){
-			continue;
-		}
-		else {
-			putchar(i);
-		}
-	}
+	int hex[10], number[10];
+
+	i=0;
+    	count=0;
+	decimal=0;
+	ch = 0;
+
+	decimal = decimal * 10;
+	decimal = decimal + (ch - '0');
+	 while(decimal != 0) {
+		hex[i] = decimal%16;
+	     	decimal= decimal/16;
+		printf("remainder is %d\n",hex[i]);
+		printf("after divided is %d\n",decimal);
+		i++;
+	        count++;
+  	}
+          	
+	 i=i-1;		
+	 for(j=0; j < count; j++)
+	 {
+		 number[j]=hex[i];
+		 i--;
+	 }	
+	 for(j=0; j < count; j++)
+	 {
+		 putchar(number[j]);
+	 } 
+	 putchar('\n');
 	
 	return 0;
 }
