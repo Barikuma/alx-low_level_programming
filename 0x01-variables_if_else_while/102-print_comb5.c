4ae82b1d93d5ece5ddb8a135f5a03a8804dac7ca
Barikuma <barikumamondelo@gmail.com>
2022-09-09 23:54:48 +0000

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,12 +22,16 @@ int main(void)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
+					putchar(' ');
 					putchar(k + '0');
 					putchar(l + '0');
 					if ((i == 9) && (j == 8) && (k == 9) && (l == 9))
 					{
+						continue;
+					}
+					else
+					{
 						putchar(',');
-						putchar(' ');
 					}
 				}
 			}
