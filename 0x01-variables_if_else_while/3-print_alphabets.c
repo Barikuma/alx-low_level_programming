03d1eb9dc1cf2366e305bb9accb3d11d14759c43
Barikuma <barikumamondelo@gmail.com>
2022-09-09 14:10:29 +0000

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 97 ; i < 123 ; i++)
+	{
+		putchar(i);
+
+	}
+	for (i = 65 ; i < 91 ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
