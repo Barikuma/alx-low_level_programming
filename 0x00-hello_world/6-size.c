16062ba66c476520e9a94537e5187237206c8fee
Barikuma <barikumamondelo@gmail.com>
2022-09-09 00:16:21 +0000

    6-size task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of a int: %d byte(s)\n", sizeof(int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long: %ld byte(s)\n", sizeof(long long));
+	printf("Size of a long long: %ld byte(s)\n", sizeof(long long int));
 	return (0);
 }
