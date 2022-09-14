b189d6345dad1645ad7141ebac3349e0cb9fca78
Barikuma <barikumamondelo@gmail.com>
2022-09-14 14:16:21 +0000

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,14 +2,16 @@
 
 /**
  * jack_bauer - prints every minute of the day
+ * @h: function parameter
+ * @m: function parameter
  *
  * Return: void
  */
 
-void hr_and_min_less_than_10(void);
-void hr10_less_min10_greater(void);
-void hr10_greater_min10_less(void);
-void hr10_greater_min10_greater(void);
+void hr_and_min_less_than_10(int h, int m);
+void hr10_less_min10_greater(int h, int m);
+void hr10_greater_min10_less(int h, int m);
+void hr10_greater_min10_greater(int h, int m);
 
 void jack_bauer(void)
 {
@@ -21,18 +23,18 @@ void jack_bauer(void)
 		while (m <= 59)
 		{
 			if ((m < 10) && (h < 10))
-				hr_and_min_less_than_10();
+				hr_and_min_less_than_10(h, m);
 			else if ((m >= 10) && (h < 10))
 			{
-				hr10_less_min10_greater();
+				hr10_less_min10_greater(h, m);
 			}
 			else if ((m < 10) && (h >= 10))
 			{
-				hr10_greater_min10_less();
+				hr10_greater_min10_less(h, m);
 			}
 			else
 			{
-				hr10_greater_min10_greater();
+				hr10_greater_min10_greater(h, m);
 			}
 			m++;
 			_putchar('\n');
@@ -43,11 +45,13 @@ void jack_bauer(void)
 
 /**
  * hr10_greater_min10_greater - prints values >= 10 hours and >= 10 minutes
+ * @h: function parameter
+ * @m: function parameter
  *
  * Return: void
  */
 
-void hr10_greater_min10_greater(void)
+void hr10_greater_min10_greater(int h, int m)
 {
 	_putchar('0' + h / 10);
 	_putchar('0' + h % 10);
@@ -58,26 +62,30 @@ void hr10_greater_min10_greater(void)
 
 /**
  * hr10_greater_min10_less - prints values >= 10 hours and < 10 minutes
+ * @h: function parameter
+ * @m: function parameter
  *
  * Return: void
  */
 
-void hr10_greater_min10_less(void)
+void hr10_greater_min10_less(int h, int m)
 {
 	_putchar('0' + h / 10);
 	_putchar('0' + h % 10);
 	_putchar(':');
 	_putchar('0' + 0);
-	_putchar('0' + m;)
+	_putchar('0' + m);
 }
 
 /**
  * hr10_less_min10_greater - prints values < 10 hours and >= 10 minutes
+ * @h: function parameter
+ * @m: function parameter
  *
  * Return: void
  */
 
-void hr10_less_min10_greater(void)
+void hr10_less_min10_greater(int h, int m)
 {
 	_putchar('0' + 0);
 	_putchar('0' + h);
@@ -88,11 +96,13 @@ void hr10_less_min10_greater(void)
 
 /**
  * hr_and_min_less_than_10 - prints values < 10 hours and < 10 minutes
+ * @h: function parameter
+ * @m: function parameter
  *
  * Return: void
  */
 
-void hr_and_min_less_than_10(void)
+void hr_and_min_less_than_10(int h, int m)
 {
 	_putchar('0' + 0);
 	_putchar('0' + h);
