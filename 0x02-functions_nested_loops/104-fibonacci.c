2b2f6e12ab190b7a698de14e462ef59802cd9e61
Barikuma <barikumamondelo@gmail.com>
2022-09-14 23:57:12 +0000

    first 98 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 fibonacci numbers
+ *
+ * Return: Always - (success)
+ */
+
+int main(void)
+{
+	unsigned long f1 = 1, f2 = 0, fnext;
+	int i;
+
+	for (i = 0 ; i < 98 ; i++)
+	{
+		fnext = f1 + f2;
+		printf("%lu", fnext);
+		if (i != 97)
+		{
+			printf(", ");
+		}
+		f2 = f1;
+		f1 = fnext;
+	}
+	printf("\n");
+	return (0);
+}
