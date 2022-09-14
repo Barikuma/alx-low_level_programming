0409a05ec9ba7d112be01447f74a60e64bb40fed
Barikuma <barikumamondelo@gmail.com>
2022-09-14 14:50:09 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,18 +26,18 @@ void print_times_table(int n)
 			}
 			else if (result >= 10 && result < 100)
 			{
-				_putchar(' ');
 				_putchar('0' + result / 10);
 				_putchar('0' + result % 10);
+				_putchar(' ');
 			}
 			else
 			{
 				int r = result % 100;
 
-				_putchar(' ');
 				_putchar('0' + result / 100);
 				_putchar('0' + r / 10);
 				_putchar('0' + r % 10);
+				_putchar(' ');
 			}
 			if (j != n)
 			{
