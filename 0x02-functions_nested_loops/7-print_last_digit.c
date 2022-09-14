3586f1f07ecc2c90c5a74c93d234410f2d37aa41
Barikuma <barikumamondelo@gmail.com>
2022-09-14 03:01:03 +0000

    print the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - gets the last digit of a number
+ * @l: function parameter
+ *
+ * Return: the last digit
+ */
+
+int print_last_digit(int l)
+{
+	return (l % 10);
+}
