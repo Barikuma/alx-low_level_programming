c9a6ff00f4d5da96df5460a85037825769fe7cc5
Barikuma <barikumamondelo@gmail.com>
2022-09-14 14:06:51 +0000

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,40 +16,82 @@ void jack_bauer(void)
 		while (m <= 59)
 		{
 			if ((m < 10) && (h < 10))
-			{
-				_putchar('0' + 0);
-				_putchar('0' + h);
-				_putchar(':');
-				_putchar('0' + 0);
-				_putchar('0' + m);
-			}
+				hr_and_min_less_than_10();
 			else if ((m >= 10) && (h < 10))
 			{
-				_putchar('0' + 0);
-				_putchar('0' + h);
-				_putchar(':');
-				_putchar('0' + m / 10);
-				_putchar('0' + m % 10);
+				hr10_less_min10_greater();
 			}
 			else if ((m < 10) && (h >= 10))
 			{
-				_putchar('0' + h / 10);
-				_putchar('0' + h % 10);
-				_putchar(':');
-				_putchar('0' + 0);
-				_putchar('0' + m);
+				hr10_greater_min10_less();
 			}
 			else
 			{
-				_putchar('0' + h / 10);
-				_putchar('0' + h % 10);
-				_putchar(':');
-				_putchar('0' + m / 10);
-				_putchar('0' + m % 10);
+				hr10_greater_min10_greater();
 			}
 			m++;
 			_putchar('\n');
 		}
 		h++;
 	}
+}
+
+/**
+ * hr10_greater_min10_greater - prints values >= 10 hours and >= 10 minutes
+ *
+ * Return: void
+ */
+
+void hr10_greater_min10_greater(void)
+{
+	_putchar('0' + h / 10);
+	_putchar('0' + h % 10);
+	_putchar(':');
+	_putchar('0' + m / 10);
+	_putchar('0' + m % 10);
+}
+
+/**
+ * hr10_greater_min10_less - prints values >= 10 hours and < 10 minutes
+ *
+ * Return: void
+ */
+
+void hr10_greater_min10_less(void)
+{
+	_putchar('0' + h / 10);
+	_putchar('0' + h % 10);
+	_putchar(':');
+	_putchar('0' + 0);
+	_putchar('0' + m;)
+}
+
+/**
+ * hr10_less_min10_greater - prints values < 10 hours and >= 10 minutes
+ *
+ * Return: void
+ */
+
+void hr10_less_min10_greater(void)
+{
+	_putchar('0' + 0);
+	_putchar('0' + h);
+	_putchar(':');
+	_putchar('0' + m / 10);
+	_putchar('0' + m % 10);
+}
+
+/**
+ * hr_and_min_less_than_10 - prints values < 10 hours and < 10 minutes
+ *
+ * Return: void
+ */
+
+void hr_and_min_less_than_10(void)
+{
+	_putchar('0' + 0);
+	_putchar('0' + h);
+	_putchar(':');
+	_putchar('0' + 0);
+	_putchar('0' + m);
 }
