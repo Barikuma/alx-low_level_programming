0f16e0c1da1787832cc1a034850e464277711c6e
Barikuma <barikumamondelo@gmail.com>
2022-09-14 14:29:11 +0000

    n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times table of n number
+ * @n: function parameter
+ *
+ * Return: void
+ */
+
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 0 ; j <= n ; j++)
+		{
+			result = i * j;
+			if (result < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+			}
+			if (j != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
