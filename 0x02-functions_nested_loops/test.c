eb7950075d930dc4ab3e676883696a8c1b4182d7
Barikuma <barikumamondelo@gmail.com>
2022-09-14 11:25:56 +0000

    print to 98

--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -1,29 +1,73 @@
 #include <stdio.h>
 #include <limits.h>
 
-void num(){
-	int i, j, rslt;
-	for (i = 0 ; i <= 9 ; i++){
-		for (j = 0 ; j <= 9; j++){
-			rslt = i * j;
-			if (rslt >= 10){
-				putchar('0' + rslt / 10);
-				putchar('0' + rslt % 10);
+void num(int n){
+	if (n >= 0 && n <= 98){
+		for (n = n ; n <= 98 ; n++){
+			if (n < 10){
+				putchar('0' + n);
 			}
-			else
-			{
-				putchar('0' + rslt);
+			else{
+				putchar('0' + n / 10);
+				putchar('0' + n % 10);
 			}
-			if (j != 9){
+			if ( n != 98){
 				putchar(',');
 				putchar(' ');
 			}
 		}
-		putchar('\n');
-	}	
-}
-int main(void){
-	num();
-	return (0);
+	}
+	else if (n > 98){
+		for (n = n ; n >=98 ; n--){
+			if (n < 100){
+				putchar('0' + n / 10);
+				putchar('0' + n % 10);
+			}
+			else{
+				int h = n % 100;
+				putchar('0' + n / 100);
+				putchar('0' + h / 10);
+				putchar('0' + h % 10);
+			}
 
+			if(n != 98){
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	else{
+		for (n = n ; n <= 98 ; n++){
+			if ((n < 0) && (n > -10)){
+				putchar('-');
+				putchar('0' + n * -1);
+			}
+			else if (n >= 10){
+				putchar('0' + n / 10);
+				putchar('0' + n % 10);
+			}
+			else {
+				putchar('0' + n);
+			}
+
+
+			if (n != 98){
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+
+		putchar('\n');
+}
+
+int main(void){
+	num(-9);
+	/*putchar('-');
+	putchar('0' + 20 / 10);
+	putchar('0' + 20 % 10);
+	putchar('\n');
+	*/
+	return 0;
 }
