8c454c758c0d5648d263177424a12cd42a9af983
Barikuma <barikumamondelo@gmail.com>
2022-09-14 15:03:02 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,44 +11,30 @@ void print_times_table(int n)
 {
 	int i, j, result;
 
-	for (i = 0 ; i <= n ; i++)
+	if (n < 15 && n > 0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		for (j = 1 ; j <= n ; j++)
+		for (i = 0 ; i <= n i++)
 		{
-			result = i * j;
-			if (result < 10)
+			_putchar('0');
+			
+			for (j = 1 ; j <= 9 ; j++)
 			{
 				_putchar(' ');
 				_putchar(' ');
-				_putchar('0' + result);
-			}
-			else if (result >= 10 && result < 100)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result / 10);
-				_putchar('0' + result % 10);
-			}
-			else
-			{
-				int r = result % 100;
 
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + result / 100);
-				_putchar('0' + r / 10);
-				_putchar('0' + r % 10);
+				result = i * j;
+
+				if (result <= 9)
+				{
+					_putchar('0' + result);
+				}
+				else
+				{
+					_putchar('0' + result / 10);
+					_putchar('0' + result % 10);
+				}
 			}
-			if (j != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
