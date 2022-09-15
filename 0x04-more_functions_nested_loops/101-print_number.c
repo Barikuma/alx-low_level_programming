56e9d50941fc50dfa9df1606688508b61c5e6f22
Barikuma <barikumamondelo@gmail.com>
2022-09-15 13:08:35 +0000

    positive or negative number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 10)
+		_putchar('0' + n);
+	else
+	{
+		_putchar('0' + n)
+	}
+}
