4c2315db1c1fc42c7011eb6204b2d9e4678cb978
Barikuma <barikumamondelo@gmail.com>
2022-09-15 15:30:09 +0000

    leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || year % 400 == 0 (&& year % 100 == 0))
+	if (year % 4 == 0 || year % 400 == 0 && year % 100 == 0)
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -31,10 +31,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			if (month != 4 && month != 9 && month != 11 && month != 6){
-				printf("Day of the year: %d\n", day);
-				printf("Remaining days: %d\n", 365 - day);
-			}
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
