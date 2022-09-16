b0ac653720f3ddb77e0033f44dfff56e6c2779b9
Barikuma <barikumamondelo@gmail.com>
2022-09-16 15:28:44 +0000

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,10 +9,12 @@
 
 void print_number(int n)
 {
-	if (n < 10)
-		_putchar('0' + n);
-	else
+	if (n < 0)
 	{
-		_putchar('0' + n)
+		_putchar('-');
+		n = -n;
 	}
+	if (n > 10)
+		print_number(n / 10);
+	_putchar('0' + (n % 10));
 }
