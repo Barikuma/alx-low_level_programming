68414dd4392549320982906da887000f7d7327be
Barikuma <barikumamondelo@gmail.com>
2022-09-19 14:38:29 +0000

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the content of an address
+ * @n: function pointer parameter, accepts an address
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
