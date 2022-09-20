e8c3482b8ccdac0b25d97c6d98f4b424429675b3
Barikuma <barikumamondelo@gmail.com>
2022-09-20 02:46:35 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n element of an array of integers
+ * @a: array
+ * @n: number of items
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n ; i++)
+		printf("%d", a[i]);
+	putchar('\n');
+}
