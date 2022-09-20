623ea4f4ad4adccfea30d9052fbc318fdda3eea6
Barikuma <barikumamondelo@gmail.com>
2022-09-20 01:32:39 +0000

    length of string

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,13 +1,19 @@
 #include "main.h"
 #include <stdio.h>
+int _strlen(char *s);
 
 int main(void)
 {
-	int a = 98, b = 42;
-
-	printf("a = %d, b = %d\n", a, b);
-	swap_int(&a, &b);
-	printf("a = %d, b = %d\n", a, b);
+	char *s = "Hello";
+	int len = _strlen(s);
+	printf("%d\n", len);
 
 	return 0;
+}
+
+int _strlen(char *s)
+{
+	int i = 0;
+	for ( ; s[i] != '\0' ; i++);
+	return i;
 }
