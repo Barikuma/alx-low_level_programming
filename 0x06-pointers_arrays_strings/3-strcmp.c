a41a7e600b8d512876dbdc03a21eafd6964b9584
Barikuma <barikumamondelo@gmail.com>
2022-09-22 19:33:07 +0000

    compare stringd

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,25 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, sum1 = 0, sum2 = 0;
-
-	int len = _strlen(s1);
-
-	for (; i < len ; i++)
+	while (*s1 && *s2)
 	{
-		if (s1[i] > s2[i])
-			sum1 += s1[i];
-		else if (s1[i] < s2[i])
-			sum2 += s2[i];
-		else
-		{
-			sum1 += s1[i];
-			sum2 += s2[i];
-		}
+		if (*s1 != *s2)
+			return (*s1++ - *s2++);
 	}
-	if (sum1 > sum2)
-		return (15);
-	if (sum1 < sum2)
-		return (-15);
-	return (0);
+	return (*s1 - *s2);
 }
