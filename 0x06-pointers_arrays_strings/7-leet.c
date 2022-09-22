fba376db8b52836db423e343f3d0f2f317e2bd91
Barikuma <barikumamondelo@gmail.com>
2022-09-22 22:45:37 +0000

    leet encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,7 +6,18 @@
  * Return: str
  */
 
-char *leet(char *)
+char *leet(char *str)
 {
-	
+	int i = 0, j;
+	char c_char[10] = {'A', 'a', 'E', 'e', 'O', 'o', 'T', 't', 'L', 'l'},
+	     c_digits[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	while (str[i])
+	{
+		for (j = 0 ; j < 10 ; j++){
+			if (str[i] == c_har[j])
+				str[i++] = c_digits[j];
+		}
+	}
+	return (str);
 }
