7d139196fb41df95739c5411de844eb1fefb4037
Barikuma <barikumamondelo@gmail.com>
2022-09-22 21:08:37 +0000

    capitalize words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 {
 	int len = _strlen(str), i = 0;
 	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
-		'"', '(', ')', '{', '}'}
+		'"', '(', ')', '{', '}'};
 
 	while (i < len)
 	{
