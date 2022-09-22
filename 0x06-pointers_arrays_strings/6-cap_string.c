3a5ecc8c578b3640ba7807285aef119183f9d4a1
Barikuma <barikumamondelo@gmail.com>
2022-09-22 20:34:19 +0000

    capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes the words of a string
+ * @str: string to capitalize words
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int len = _strlen(str), i = 0;
+
+	while (i < len)
+	{
+		if (str[i++] == ' ')
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
