b5e07e87885e6eac12f0d7ba24eb34e29ff80f0d
Barikuma <barikumamondelo@gmail.com>
2022-09-22 23:59:24 +0000

    cp

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, dlen;
-
-	dlen = _strlen(dest);
-
+	int dest_len, i;
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	;
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dlen + i] = src[i];
-	dest[dest_len + i] = '\0';
+		dest[dest_len + i] = src[i];
+
+	dest[dest_len + i] = src[i];
 
 	return (dest);
 }
