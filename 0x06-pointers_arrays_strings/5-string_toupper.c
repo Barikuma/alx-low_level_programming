ac9fbb018f195cae6941af09c0c97e59761359cd
Barikuma <barikumamondelo@gmail.com>
2022-09-22 20:15:59 +0000

    to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,10 +11,8 @@ char *string_toupper(char *str)
 	int i = 0, len = _strlen(str);
 	while (i < len && str[i] = '\0')
 	{
-		if (str[i] >= 65 && str[i] <= 90)
-			continue;
-		else
-			str[i] = str[i] - 32;
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] + 32;
 		i++;
 	}
 	return (str);
