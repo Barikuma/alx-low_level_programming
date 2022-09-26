3128323d3b295775d9d5d645322c9d5824a92d66
Barikuma <barikumamondelo@gmail.com>
2022-09-26 15:29:31 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to search through
+ * @accept: the string you wish to find
+ * Return: the length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int i = 0, j, a_len = 0;
+
+	while (accept[a_len] != '\0')
+		a_len++;
+
+	while (a_len > 0)
+	{
+
+		for (j = 0 ; j < a_len ; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				count++;
+				break;
+			}
+		}
+		i++;
+		a_len--;
+	}
+	return (count);
+}
--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -4,18 +4,15 @@ char *mcpy(char *dest, char *src, int n);
 
 int main(void)
 {
-	char a[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+	char a[] = "abcdefghij";
 	char b[] = {'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y'};
-	int i;
+	int i = 0;
 
-	for (i = 0 ; i < 10 ; i++)
-		printf("%c", a[i]);
-	putchar('\n');
-	
-	mcpy(a+5, b, 5);
-	for (i = 0 ; i < 10 ; i++)
-		printf("%c", a[i]);
-	putchar('\n');
+	while (a[i] != '\0')
+	{
+		i++;
+	}
+	printf("%d\n", i);
 
 	return (0);
 }
