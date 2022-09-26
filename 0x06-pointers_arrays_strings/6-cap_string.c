a57c61612d4f42891a310a04089fc10161f29179
Barikuma <barikumamondelo@gmail.com>
2022-09-26 05:39:00 +0000

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		if (str[i] == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+		if ((i == 0) && (str[i] >= 'a' && str[i] <= 'z'))
 			str[i] -= 32;
 		for (j = 0 ; j < 13 ; j++)
 		{
