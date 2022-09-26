c0f45b3ff3b2ef693d9884e4e52d0b5bf30ebead
Barikuma <barikumamondelo@gmail.com>
2022-09-26 19:30:01 +0000

    strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,4 +5,15 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+int straccept(char c, char *accept)
+{
+	int i;
+
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (c == accept[i])
+			return (1);
+	}
+	return (0);
+}
 #endif
