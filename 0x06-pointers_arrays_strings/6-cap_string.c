373e6ddfe635ec6b4416704c751a727763873dc0
Barikuma <barikumamondelo@gmail.com>
2022-09-26 05:31:21 +0000

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,45 +1,31 @@
 #include "main.h"
 
-int check_seperators(char c);
-
 /**
- * cap_string - capitalizes the words of a string
- * @str: string to capitalize words
+ * cap_string - capitalizes the first letter of a word
+ * @str: string
  * Return: str
  */
 
 char *cap_string(char *str)
 {
-	int i = 0;
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i = 0, j;
+	char c;
 
-	while (s[i])
+	while (str[i])
 	{
-		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
-			s[i] -= 32;
-
-		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
-			s[i + 1] -= 32;
+		if (str[i] == 0 && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+		for (j = 0 ; j < 13 ; j++)
+		{
+			c = sep[j];
+			if (c = str[i] && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+			{
+				str[i + 1] -= 32;
+				break;
+			}
+		}
 		i++;
 	}
-	return (str);
-}
-
-/**
- * check_seperators - Separators of words: space, tabulation, new line,
- * ,, ;, ., !, ?, ", (, ), {, and }
- * @c: an input character
- * Return: 1 if seperator, 0 otherwise
- */
-
-int check_seperators(char c)
-{
-	int i = 0;
-	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
-		'"', '(', ')', '{', '}' };
-	for (; i < 13; i++)
-	{
-		if (c == seperators[i])
-			return (1);
-	}
-	return (0);
+	return (str)
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,12 +1,32 @@
-#include "main.h"
 #include <stdio.h>
+
+char *cap_string(char *str);
 
 int main(void)
 {
-	char s1[] = "hello ";
-	char s2[] = "world\n";
+	char s1[] = "hello it is Me";
 
-	_strncat(s1, s2, 4);
+	printf("%s\n", cap_string(s1));
 
 	return 0;
+}
+
+char *cap_string(char *str)
+{
+	int i = 0, len = 0, j = 0;
+
+	while (str[j] != '\0')
+	{
+		len++;
+		j++;
+	}
+	
+	while (i < len && str[i] != '\0')
+	{
+		if (str[i] == ' ')
+			str[i + 1] = str[i + 1] - 32;
+		i++;
+	}
+
+	return str;
 }
