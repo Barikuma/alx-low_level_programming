5e4d726b1b04288ad73160110a1a81852f82cd89
Barikuma <barikumamondelo@gmail.com>
2022-09-28 15:47:17 +0000

    reverse string recursuion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-/*
+
 int _strlen(char *s)
 {
 	int len = 0;
@@ -14,4 +14,4 @@ int _strlen(char *s)
 		len++;
 	return (len);
 }
-*/
+
