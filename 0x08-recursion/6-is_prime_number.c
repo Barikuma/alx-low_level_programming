05cbf46644f6053e7c08f630ece408c3b1789385
Barikuma <barikumamondelo@gmail.com>
2022-09-28 15:30:52 +0000

    is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,25 +6,17 @@
  * Return: 1 if the number is and 0 if not
  */
 
-int _is_prime(int n);
-
 int is_prime_number(int n)
-{
-	if (_is_prime(n))
-		return (1);
-	return (0);
-}
-
-int _is_prime(int n)
 {
 	int sum = 0;
 
-	if((n <= 0) || n == 1)
-	       return (0);
-
-	sum = (n % 10) + _is_prime(n / 10);
+	if ((n <= 0) || n == 1)
+		return (0);
+	sum = (n % 10) + is_prime_number(n / 10);
 
 	if (((n % 10) % 2 == 0) && ((sum % 3) == 0))
 		return (0);
 	return (1);
 }
+
+
