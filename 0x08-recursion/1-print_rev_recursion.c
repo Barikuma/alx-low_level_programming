f9a1fa3cd78f10c3c2d4d031b1de841206f78687
Barikuma <barikumamondelo@gmail.com>
2022-09-28 15:50:13 +0000

    reverse string recursuion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,12 +11,8 @@ void _print_rev_recursion(char *s)
 	int len = _strlen(s);
 
 	if (len < 0)
-	{
 		return;
-	}
-
 	len--;
-	s = s + len;
-	_print_rev_recursion(s - 1);
-	_putchar(*s);
+	_putchar(s[len]);
+	_print_rev_recursion(s + 1);
 }
