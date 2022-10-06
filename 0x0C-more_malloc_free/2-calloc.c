02e3e62500a96f6db31c3fa11049b347c65bb8a4
Barikuma <barikumamondelo@gmail.com>
2022-10-06 20:24:21 +0000

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	ptr = malloc(nmemb * size);
 
-	if(ptr == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
