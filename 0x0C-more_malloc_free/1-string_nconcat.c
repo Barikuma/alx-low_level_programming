ce2119721d3c266b92b77265c1d2ab5d0d7917e1
Barikuma <barikumamondelo@gmail.com>
2022-10-06 13:43:56 +0000

    string nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * string_nconcat - concatenates s1 with s2 for a number of n bytes
+ * @s1: string to append
+ * @s2: string to concatenate with
+ * Return: address of the concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1_len, s2_len, i;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
+
+	if (n >= s2_len)
+		n = s2_len;
+
+	str = malloc(s1_len);
+	
+	if (str == NULL)
+		return (NULL);
+
+	str = realloc(str, (s1_len + n));
+
+	if (str == NULL)
+		return (NULL);
+	
+	str = str + s1_len;
+
+	for (i = 0; i < n; i++)
+	{
+		*str = s2[i];
+		str++;
+	}
+
+	return (str);
+
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+int _strlen(char *s)
+{
+	int len = 0;
+	
+	while (*(s++) != '\0')
+		len++;
+	return (len);
+}
 #endif
