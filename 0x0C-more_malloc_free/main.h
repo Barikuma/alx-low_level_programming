fd573be7a87d0b7e0b0f8707bad314ceb3082ebe
Barikuma <barikumamondelo@gmail.com>
2022-10-06 14:56:50 +0000

    string_nconcat

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,12 +3,12 @@
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
-/*int _strlen(char *s)
+*int _strlen(char *s)
 {
 	int len = 0;
 	
 	while (*(s++) != '\0')
 		len++;
 	return (len);
-}*/
+}
 #endif
