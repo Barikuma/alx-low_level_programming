18e9e4302e5be5026586f4edf18ec269d4f2d57f
Barikuma <barikumamondelo@gmail.com>
2022-10-10 21:16:43 +0000

    init dog update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,14 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+ * init_dog - initializes a dog struct
+ * @d: struct pointer
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner's name
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
