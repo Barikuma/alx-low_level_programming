c756b2c21bc36aed05c4a900a045a5d5f034ddc1
Barikuma <barikumamondelo@gmail.com>
2022-10-10 22:21:37 +0000

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner's name
+ *
+ * Return: a pointer
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t doggo;
+	dog_t *d_ptr = &doggo;
+
+	doggo.name = name;
+	doggo.age = age;
+	doggo.owner = owner;
+
+	return (d_ptr);
+}
