17d8d8e966fa8c89217aeceffd157c3c67a69efe
Barikuma <barikumamondelo@gmail.com>
2022-10-12 18:49:38 +0000

    search int

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,36 @@
+#include "functon_pointers.h"
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: the array to iterate
+ * @size: size of the array
+ * @cmp: function pointer
+ * Return: -1 if size <= 0 or if the integer wasn't found,
+ * else the index of the number
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !cmp)
+	{
+
+		return (0);
+
+	}
+
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
