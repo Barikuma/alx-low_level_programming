4591d738d97c032deed294689b2b814ce49dc05a
Barikuma <barikumamondelo@gmail.com>
2022-10-13 09:51:24 +0000

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all its parameters
+ * @n: numbers to sum
+ * Return: the sum of all parameters, if n is 0 return 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list arg;
+	u_int i, sum = 0;
+	
+	if (n != 0)
+	{
+		va_start(arg, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(arg, u_int);
+		}
+		va_end(arg);
+		return (sum);
+	}
+	return (0);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,5 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+typedef unsigned int u_int;
+int sum_them_all(const unsigned int n, ...);
+#endif
