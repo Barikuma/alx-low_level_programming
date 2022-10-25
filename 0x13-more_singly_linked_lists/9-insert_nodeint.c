db7d48609e4673f94d04329afaeba30fcd034884
Barikuma <barikumamondelo@gmail.com>
2022-10-25 23:06:29 +0000

    insert node update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	
 	ptr = *head;
 
-	if (ptr == NULL && index != 0)
+	if (ptr == NULL && idx != 0)
 		return (NULL);
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		node->next = *head;
-		*head = *node;
+		*head = node;
 		return (node);
 	}
 
