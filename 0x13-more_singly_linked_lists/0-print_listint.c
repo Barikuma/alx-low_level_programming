3c219d5421dd3aa0dfd51b099bf023f97d1f8fc7
Barikuma <barikumamondelo@gmail.com>
2022-10-25 21:27:20 +0000

    print listint update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,8 +12,10 @@ size_t print_listint(const listint_t *h)
 	size_t i = 0;
 
 	while (h == NULL)
-		return (i);
-	printf("%d\n", h->n);
-	i = print_listint(h->next);
-	return (++i);
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
