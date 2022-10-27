3eb0bf6c75092c29afb30123ee59f42c82360160
Barikuma <barikumamondelo@gmail.com>
2022-10-27 02:14:16 +0000

    101-update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,6 +16,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listadd_s - singly linked list
+ * @address: Address of a node in a list
+ * @next: Pointer to next node
+ */
+typedef struct listadd_s
+{
+	void *address;
+	struct listadd_s *next;
+} listadd_t;
+void free_listadd(listadd_t *head);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
